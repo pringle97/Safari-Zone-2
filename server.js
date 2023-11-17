ae908184bec4515e9ca1e63c308a5309b9bb3116
Malia Pringle <malia.pringle@gmail.com>
2023-11-17 14:33:29 -0800

    adding connection to mongodb

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+// const routes = require("./routes")
+
+// const PORT = process.env.PORT || 3001;
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// app.use(routes)
+
+require("./db")
+  .then(() => app.listen(3001))
+  .catch(err => console.log(err))
